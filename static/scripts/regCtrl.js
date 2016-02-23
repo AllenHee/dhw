a85ecf1b84d49aca7ac67a4e0ac2bcbba3074d49
He Wei <hw961919531@outlook.com>
2016-02-23 17:31:44 +0800

    注册页面bug

--- static/scripts/regCtrl.js
+++ static/scripts/regCtrl.js
@@ -1,34 +1,34 @@
 var app = angular.module('regApp', ['baseapp']);
 
-app.controller('regCtrl', ['$scope', '$http', function(s, h) {
+app.controller('regCtrl', ['$scope', '$http', function (s, h) {
   s.dataP = {};
-  s.dataC= {};
+  s.data = {};
   var user = $('#user').val();
   var userpwd = $('#userpwd').val();
   // var usercon = $('#usercon').val();
   var company = $('#company').val();
   var compwd = $('#compwd').val();
   // var comcon = $('#comcon').val();
-  if(user) {
+  if (user) {
     s.dataP.name = user;
   }
-  if(userpwd) {
+  if (userpwd) {
     s.dataP.pwd = userpwd;
     s.confirmP = userpwd;
   }
-  if(company) {
-    s.dataC.name = company;
+  if (company) {
+    s.data.name = company;
   }
-  if(compwd) {
-    s.dataC.pwd = compwd;
+  if (compwd) {
+    s.data.pwd = compwd;
     s.confirmC = compwd;
   }
-  
-  
+
+
   s.isPersonal = true;
   s.valiType = 0;
   s.isValid = {};
-  
+
   s.list = {
     departments: ['办公室', '市场部', '采购部', '技术部', '人力资源', '其他'],
     provinces: _areaselect_data.p,
@@ -39,36 +39,37 @@ app.controller('regCtrl', ['$scope', '$http', function(s, h) {
     natures: ['政府机关/事业单位', '国营', '私营', '中外合资', '外资', '其他']
   };
   
-  s.getCitys = function(prov) {
-    if (prov.indexOf('市') > 0) {
-      var arr = [];
-      arr.push(prov);
-      s.list.citys = arr;
-    } else {
-      s.list.citys = _areaselect_data.c[prov];
+  h.post('/UserAccount/Company').success(function (data) {
+    var p = data.result.province;
+
+    if (p) {
+      var c = data.result.city;
+      s.area.province = p
+      s.areaList.cities = _areaselect_data.c[p];
+      s.areaShow.showCities = true;
+      if (!_areaselect_data.s[p]) {
+        s.areaList.counties = _areaselect_data.a[p + '-' + c];
+        s.areaShow.showCounties = true;
+      }
+
+      setTimeout(function () {
+        s.area.city = c;
+        s.area.county = data.result.addr;
+      });
     }
-    s.list.counties = [];
-    s.dataC.city= '';
-    s.dataC.county = '';
-  };
-  s.getCounties = function(city) {
-    if (city.split('-')[0] == city.split('-')[1]) {
-      s.list.counties = _areaselect_data.c[city.split('-')[0]];
-    } else {
-      s.list.counties = _areaselect_data.a[city];
-    }
-    s.dataC.county = '';
-  };
-  
-  h.post('/HRZpxxFb/Tradeinfo').success(function(data) {
+
+    $.extend(s.data, data.result);
+  });
+
+  h.post('/HRZpxxFb/Tradeinfo').success(function (data) {
     s.list.trades = data.result;
   });
   
   
   // 验证用户名、手机、邮箱
-  s.validate = function(value, prop) {
+  s.validate = function (value, prop) {
     if (value != undefined) {
-      h.post('/Reg/Check', {keyword: value}).success(function(data) {
+      h.post('/Reg/Check', { keyword: value }).success(function (data) {
         if (data.result) {
           s.isValid[prop] = 1;
         } else {
@@ -79,40 +80,41 @@ app.controller('regCtrl', ['$scope', '$http', function(s, h) {
   };
   
   // 刷新验证码
-  s.codeRefresh = function() {
+  s.codeRefresh = function () {
     $('.verifyImg').attr('src', '/Login/VerifyCode?_=' + Math.random());
   };
   
   // 关闭验证码错误提示框
-  s.closeErrorPop = function() {
+  s.closeErrorPop = function () {
     $('.errorPop_wrap, .errorPop').hide();
   };
-  
-  s.submit = function() {
+
+  s.submit = function () {
     var para;
     var url;
     if (s.isPersonal) {
       para = $.extend({}, s.dataP);
       url = '/Reg/AddPerson'
     } else {
-      para = $.extend({}, s.dataC);
+      para = $.extend({}, s.data);
       url = '/Reg/AddCompany';
     }
-    
-    h.post(url, para).success(function(data) {
+
+    h.post(url, para).success(function (data) {
       if (data.success) {
         $('.errorPop_wrap').show();
         $('.successPop').show();
-        setTimeout(function() {
-          location.pathname =  "/login";
+        setTimeout(function () {
+          location.pathname = "/login";
         }, 3000)
-      }else {
+      } else {
         $('.errorPop_wrap').show();
-        $('.errorPop').fadeIn(function() {
+        $('.verifyCodeError').html(data.msg)
+        $('.errorPop').fadeIn(function () {
           s.codeRefresh();
         });
       }
     });
-    
+
   }
 }]);
